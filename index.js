d79f34d6cceed9866d8ac24c79032552eb807f4c
chuksokpani <61967909+chuksokpani@users.noreply.github.com>
2020-03-27 00:17:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let firstName = 'Chukwukere';
+let lastName = 'Okpani';
+let courses = ['HTML', 'CSS', 'JavaScript'];
+console.log(firstName + ' ' + lastName);
+console.log(courses);
+for (let count = 1; count <= 200; count++) {
+    if (count % 2 == 1) {
+        console.log(count);
+        
+    };
+    
+}
+
+
+
